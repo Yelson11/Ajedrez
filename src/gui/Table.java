72a382639588250ba09e99ffaed0919c6fe26577
Damazio <edamazzio@gmail.com>
2020-06-24 22:58:19 -0600

    Bug de solo líneas verticales arreglado

--- src/gui/Table.java
+++ src/gui/Table.java
@@ -17,78 +17,70 @@ import javax.swing.JPanel;
 import juego.tablero.tableroUtilitarios;
 
 public class Table {
-    
+
     private final JFrame gameFrame;
     private final BoardPanel boardPanel;
     private Color lightTileColor = Color.decode("#FFFACD");
     private Color darkTileColor = Color.decode("#593E1A");
-    
-    private final static Dimension OUTER_FRAME_DIMENSION = new Dimension(600,600);
+
+    private final static Dimension OUTER_FRAME_DIMENSION = new Dimension(600, 600);
     private final static Dimension BOARD_PANEL_DIMENSION = new Dimension(400, 350);
     private final static Dimension TILE_PANEL_DIMENSION = new Dimension(10, 10);
-    
-    public Table(){
+
+    public Table() {
         this.gameFrame = new JFrame("Chess");
         this.gameFrame.setLayout(new BorderLayout());
         final JMenuBar tableMenuBar = createTableMenuBar();
         this.gameFrame.setSize(OUTER_FRAME_DIMENSION);
-        
+
         this.boardPanel = new BoardPanel();
         this.gameFrame.add(this.boardPanel, BorderLayout.CENTER);
-        
+
         this.gameFrame.setVisible(true);
     }
-    
-    private JMenuBar createTableMenuBar(){
+
+    private JMenuBar createTableMenuBar() {
         final JMenuBar tableMenuBar = new JMenuBar();
         tableMenuBar.add(createFileMenu());
         return tableMenuBar;
     }
-    
-    private JMenu createFileMenu()
-    {
+
+    private JMenu createFileMenu() {
         final JMenu fileMenu = new JMenu("File");
         final JMenuItem openPGN = new JMenuItem("Load PGN File");
-        openPGN.addActionListener(new ActionListener()
-        {
+        openPGN.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e)
-            {
+            public void actionPerformed(ActionEvent e) {
                 System.out.println("open up that pgn file");
             }
-        
+
         });
         fileMenu.add(openPGN);
         return fileMenu;
     }
-    
-    private class BoardPanel extends JPanel
-    {
+
+    private class BoardPanel extends JPanel {
         final List<TilePanel> boardTiles;
-        
-        BoardPanel()
-        {
-            super(new GridLayout(8,8));
+
+        BoardPanel() {
+            super(new GridLayout(8, 8));
             this.boardTiles = new ArrayList<>();
-            for(int i=0; i < tableroUtilitarios.NUM_CASILLAS; i++)
-            {
+            for (int i = 0; i < tableroUtilitarios.NUM_CASILLAS; i++) {
                 final TilePanel tilePanel = new TilePanel(this, i);
                 this.boardTiles.add(tilePanel);
                 add(tilePanel);
-                
+
             }
             setPreferredSize(BOARD_PANEL_DIMENSION);
             validate();
-            
+
         }
     }
-    
-    private class TilePanel extends JPanel
-    {
+
+    private class TilePanel extends JPanel {
         private final int tileId;
-        
-        TilePanel (final BoardPanel boardPanel, final int tileId)
-        {
+
+        TilePanel(final BoardPanel boardPanel, final int tileId) {
             super(new GridBagLayout());
             this.tileId = tileId;
             setPreferredSize(TILE_PANEL_DIMENSION);
@@ -97,16 +89,20 @@ public class Table {
         }
 
         private void assignTileColor() {
-            
-            if(tableroUtilitarios.FIRST_ROW[this.tileId] || tableroUtilitarios.THIRD_ROW[this.tileId] || tableroUtilitarios.FIFTH_ROW[this.tileId] || tableroUtilitarios.SEVENTH_ROW[this.tileId])
-            {
+
+            if (tableroUtilitarios.FIRST_ROW[this.tileId] || 
+                tableroUtilitarios.THIRD_ROW[this.tileId] || 
+                tableroUtilitarios.FIFTH_ROW[this.tileId] || 
+                tableroUtilitarios.SEVENTH_ROW[this.tileId]) {
                 setBackground(this.tileId % 2 == 0 ? lightTileColor : darkTileColor);
-            } else             if(tableroUtilitarios.SECOND_ROW[this.tileId] || tableroUtilitarios.FOURTH_ROW[this.tileId] || tableroUtilitarios.SIXTH_ROW[this.tileId] || tableroUtilitarios.EIGHTH_ROW[this.tileId])
-            {
-                setBackground(this.tileId % 2 == 0 ? lightTileColor : darkTileColor);
+            } else if ( tableroUtilitarios.SECOND_ROW[this.tileId] || 
+                        tableroUtilitarios.FOURTH_ROW[this.tileId] || 
+                        tableroUtilitarios.SIXTH_ROW[this.tileId] || 
+                        tableroUtilitarios.EIGHTH_ROW[this.tileId]) {
+                setBackground(this.tileId % 2 != 0 ? lightTileColor : darkTileColor);
             }
-            
+
         }
     }
-    
+
 }
