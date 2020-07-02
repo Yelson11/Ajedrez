cd674b46f5aa210735f670e543a007cc2f5478fc
Yelson11 <yelsonmonge@gmail.com>
2020-07-02 14:43:30 -0600

    MiniMax - Board Score

--- src/juego/jugador/ia/StandardBoardEvaluator.java
+++ src/juego/jugador/ia/StandardBoardEvaluator.java
@@ -14,6 +14,11 @@ import juego.tablero.Board;
  * @author Yelson
  */
 public class StandardBoardEvaluator implements BoardEvaluator {
+    
+    private static final int CHECK_BONUS = 50;
+    private static final int CHECK_MATE_BONUS = 10000;
+    private static final int DEPTH_BONUS = 100;
+    private static final int CASTLE_BONUS = 60;
 
     public StandardBoardEvaluator() {
     }
@@ -24,9 +29,30 @@ public class StandardBoardEvaluator implements BoardEvaluator {
     }
 
     private int scorePlayer(final Board board, final Player player, final int depth) {
-        return pieceValue(player);
+        return pieceValue(player) + mobility(player) + check(player) + checkmate(player, depth) + castled(player);
         // + checkmate, check, castled, mobility...
     }
+    
+    private int mobility(final Player player){
+        return player.getLegalMovements().size();
+    }
+    
+    private static int castled(final Player player){
+        return player.isCastled() ? CASTLE_BONUS : 0;
+    }
+    
+    private static int check(Player player) {
+        return player.getOponent().isInCheck() ? CHECK_BONUS : 0;
+    }
+    
+    private int checkmate(Player player, int depth) {
+        return player.getOponent().isInCheckMate() ? CHECK_MATE_BONUS * depthBonus(depth) : 0;
+    }
+    
+    private static int depthBonus(int depth){
+        return depth == 0 ? 1 : DEPTH_BONUS * depth;
+    }
+
     
     private static int pieceValue(final Player player){
         int piecesValueScore = 0;
@@ -35,6 +61,5 @@ public class StandardBoardEvaluator implements BoardEvaluator {
         }
         return piecesValueScore;
     }
-
     
 }
